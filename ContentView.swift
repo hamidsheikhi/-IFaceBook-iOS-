3ce94c3e68774703cc3c823fccd1a042fe8c26ad
hamidsheikhi <62965248+hamidsheikhi@users.noreply.github.com>
2020-08-01 00:50:58 +0200

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IFaceBook
+//
+//  Created by hamid sheikhi on 2020. 07. 16..
+//  Copyright © 2020. hamid sheikhi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MainControler.swift
@@ -0,0 +1,207 @@
+//
+//  MainControler.swift
+//  IFaceBook
+//
+//  Created by hamid sheikhi on 2020. 07. 16..
+//  Copyright © 2020. hamid sheikhi. All rights reserved.
+//
+
+import UIKit
+import LBTATools
+
+class PostCell: LBTAListCell<String> {
+    
+    let imageView = UIImageView(backgroundColor: .black)
+    let nameLabel = UILabel(text: "Steve Job")
+    let dateLabel = UILabel(text: "Friday at 11:11 AM")
+    let postTextLabel = UILabel(text: "I'm as proud of what we don't do as I am of what we do.")
+//  let imageViewGrid = UIView(backgroundColor: .orange)
+    
+    let photosGridController = PhotosGridController()
+    
+     override func setupViews() {
+        backgroundColor = .white
+        
+        stack(hstack(imageView.withHeight(40).withWidth(40), stack(nameLabel, dateLabel),
+                     spacing: 8
+            ).padLeft(12).padRight(12).padTop(12),
+              postTextLabel,
+              photosGridController.view,
+              spacing: 8)
+    }
+}
+class StoryHeader: UICollectionReusableView {
+    
+    let storiesController = StoriesController(scrollDirection:
+        .horizontal)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .yellow
+        
+        stack(storiesController.view)
+    }
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
+class StoryPhotoCell: LBTAListCell<String> {
+    
+    override var item: String! {
+        didSet {
+            imageView.image = UIImage(named: item)
+            }
+        }
+    
+    let imageView = UIImageView(image: nil, contentMode: .scaleAspectFill)
+    let nameLabel = UILabel(text: "Steve Jobs", font: .boldSystemFont(ofSize: 14), textColor: .white)
+    
+    override func setupViews() {
+       
+        
+        imageView.layer.cornerRadius = 10
+        
+        stack(imageView)
+        
+        setupGradientLayer()
+        
+        stack(UIView(), nameLabel).withMargins(.allSides(8))
+    }
+    
+     let gradientLayer = CAGradientLayer()
+    
+        fileprivate func setupGradientLayer() {
+            
+       
+    gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+            gradientLayer.locations = [0.7, 1.1]
+            layer.cornerRadius = 10
+            clipsToBounds = true
+            
+            layer.addSublayer(gradientLayer)
+            
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        gradientLayer.frame = bounds
+    }
+}
+
+
+
+class StoriesController: LBTAListController<StoryPhotoCell, String>, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView( _ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: 100, height: view.frame.height - 24)
+    }
+    func collectionView( _ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+    return .init(top: 0, left: 12, bottom: 0, right: 12)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.items = ["Steve2", "Steve4","Steve1", "Steve3","Steve5"]
+    }
+    
+}
+
+
+
+class MainController: LBTAListHeaderController<PostCell, String, StoryHeader> , UICollectionViewDelegateFlowLayout{
+    
+    func collectionView( _ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .init(width: 0, height: 200)
+    }
+    
+    func collectionView( _ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 12, left: 0, bottom: 0, right: 0)
+    }
+    
+    
+    
+    
+     override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .init(white: 0.9, alpha: 1)
+        
+        self.items = ["hello", "Hamid", "1", "2"]
+        setupNavBar()
+    }
+    let fbLogoImageView = UIImageView(image: UIImage(named: "fb_logo"),
+                                      contentMode: .scaleAspectFit);         let searchButton = UIButton(title: "Search", titleColor: .black)
+
+    
+    fileprivate func setupNavBar() {
+        let coverWhiteview = UIView(backgroundColor: .white)
+        view.addSubview(coverWhiteview)
+        coverWhiteview.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor)
+        let safeAreaTop = UIApplication.shared.windows.filter {$0.isKeyWindow}.first?.safeAreaInsets.top ?? 0
+        coverWhiteview.constrainHeight(safeAreaTop)
+        
+        
+        let width = view.frame.width - 120 - 16 - 60
+        
+        let titleView = UIView(backgroundColor: .clear)
+        titleView.frame = .init(x: 0, y: 0, width: width, height: 50)
+        
+
+        
+        titleView.hstack(fbLogoImageView.withWidth(120), UIView(backgroundColor: .white).withWidth(width), searchButton.withWidth(60))
+        
+            navigationItem.titleView = titleView
+
+    }
+    
+    override func scrollViewDidScroll( _ scrollView: UIScrollView) {
+        
+        let safeAreaTop = UIApplication.shared.windows.filter{$0.isKeyWindow}.first?.safeAreaInsets.top ?? 0
+        
+        let magicalSafeAreaTop: CGFloat = safeAreaTop + (navigationController?.navigationBar.frame.height ?? 0)
+        print(scrollView.contentOffset.y)
+        
+        let offset = scrollView.contentOffset.y + magicalSafeAreaTop
+        
+        let alpha: CGFloat = 1 + ((scrollView.contentOffset.y + magicalSafeAreaTop) /  magicalSafeAreaTop)
+        
+        [fbLogoImageView, searchButton].forEach{$0.alpha = alpha}
+        
+        fbLogoImageView.alpha = alpha
+        
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
+    }
+    
+   
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 400)
+    }
+}
+
+import SwiftUI
+struct MainPreview: PreviewProvider {
+    static var previews: some View {
+     //   Text("main preview")
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        func makeUIViewController(context: UIViewControllerRepresentableContext<MainPreview.ContainerView>) -> UIViewController {
+            return UINavigationController(rootViewController: MainController())
+            
+            }
+    
+        
+        
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+        }
+        
+    }
+}
